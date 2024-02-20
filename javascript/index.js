c11d127aed77a520bf6e53986dd28a7851d47833
AXYS-Creative <axyscreativeagency@gmail.com>
2024-02-19 17:14:01 -0700

    js media queries, xxl mixin, asset folders

--- javascript/index.js
+++ javascript/index.js
@@ -15,9 +15,24 @@ import "./nav.js";
 })();
 
 // Get Current Year for Copyright
-(function getCurrentYear() {
-  const yearText = document.querySelector(".year-text");
-  const currentYear = new Date().getFullYear();
+// (function getCurrentYear() {
+//   const yearText = document.querySelector(".year-text");
+//   const currentYear = new Date().getFullYear();
 
-  yearText.innerHTML = currentYear;
-})();
+//   yearText.innerHTML = currentYear;
+// })();
+
+// Media Queries
+const minMediaMD = window.matchMedia('(min-width: 768px)');
+
+function handleMediaQueryChange(e) {
+  if (e.matches) {
+    console.log('The screen is big.');
+  } else {
+    console.log('The screen is small.');
+  }
+}
+
+minMediaMD.addEventListener(handleMediaQueryChange);
+
+handleMediaQueryChange(minMediaMD);
