40ac4273e1d9aad663f2b2a0ad0028976aa8a407
Aaron Garcia <axyscreativeagency@gmail.com>
2024-04-06 15:25:41 -0600

    blog cleanup

--- netlify/functions/getAdminUser.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-  const adminUser = process.env.ADMIN_USER; // Access the environment variable
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ adminUser }),
-  };
-};
