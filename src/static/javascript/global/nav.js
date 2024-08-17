0859714e7fd5fdf2bc7228bd0fa24d03056e3624
Aaron Garcia <axyscreativeagency@gmail.com>
2024-08-16 22:11:37 -0600

    footer updates

--- src/static/javascript/global/nav.js
+++ src/static/javascript/global/nav.js
@@ -9,16 +9,19 @@ const navMenu = document.querySelector(".nav-menu"),
 
 tabElementsNav.forEach((elem) => elem.setAttribute("tabIndex", "-1"));
 
+let isNavOpen;
+
 const toggleNav = () => {
-  const isNavOpen = navMenu.classList.contains("menu-active");
   navMenu.classList.toggle("menu-active");
   menuBtn.classList.toggle("menu-active");
   siteHeader.classList.toggle("menu-active");
 
-  navMenu.setAttribute("aria-hidden", isNavOpen);
-  menuBtn.setAttribute("aria-expanded", !isNavOpen);
+  isNavOpen = navMenu.classList.contains("menu-active");
 
-  //   Update tabindex for tabElementsPage and tabElementsNav
+  navMenu.setAttribute("aria-hidden", !isNavOpen);
+  menuBtn.setAttribute("aria-expanded", isNavOpen);
+
+  // Update tabindex for tabElementsPage and tabElementsNav
   tabElementsPage.forEach((el) =>
     el.setAttribute("tabindex", isNavOpen ? "0" : "-1")
   );
@@ -27,11 +30,28 @@ const toggleNav = () => {
   );
 
   // Pevent scroll when nav is open
-  document.body.style = `overflow: ${!isNavOpen ? "hidden" : "auto"}`;
+  document.body.style = `overflow: ${isNavOpen ? "hidden" : "auto"}`;
 };
 
+// Toggle nav and allow scrolling when resizing browser greater than 1024px
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1024 && isNavOpen) {
+    navMenu.classList.remove("menu-active");
+    menuBtn.classList.remove("menu-active");
+    siteHeader.classList.remove("menu-active");
+
+    document.body.style.overflow = "auto";
+
+    navMenu.setAttribute("aria-hidden", true);
+    menuBtn.setAttribute("aria-expanded", false);
+    tabElementsPage.forEach((el) => el.setAttribute("tabindex", "0"));
+    tabElementsNav.forEach((el) => el.setAttribute("tabindex", "0"));
+
+    isNavOpen = false;
+  }
+});
+
 const closeNav = () => {
-  // Pevent scroll when nav is open
   document.body.style = "overflow: auto;";
 
   navMenu.classList.remove("menu-active");
--- src/static/javascript/utility.js
+++ src/static/javascript/utility.js
@@ -1,8 +1,18 @@
 // Get Current Year for Copyright
-// (function getCurrentYear() {
-//   const yearText = document.querySelector(".year-text");
-//   const currentYear = new Date().getFullYear();
+const getCurrentYear = (() => {
+  const yearText = document.querySelector(".year-text");
+  const currentYear = new Date().getFullYear();
 
-//   yearText.innerHTML = currentYear;
-//   yearText.setAttribute("datetime", currentYear);
-// })();
+  yearText.innerHTML = currentYear;
+  yearText.setAttribute("datetime", currentYear);
+})();
+
+// For any 'return to top' link
+const handleReturnToTop = (() => {
+  const returnToTop = document.querySelector(".return-to-top"),
+    logo = document.querySelector(".header-logo");
+
+  returnToTop.addEventListener("click", (e) => {
+    logo.focus();
+  });
+})();
