4fbd32721ce2c9b17cb6c24395d5f475ca5a3652
Aaron Garcia <axyscreativeagency@gmail.com>
2025-05-19 08:05:39 -0600

    removed swup, restored load

--- src/static/javascript/util.js
+++ src/static/javascript/util.js
@@ -11,8 +11,6 @@ export const lenis = new Lenis({
   autoRaf: true,
 });
 
-const swup = new Swup(); // Initializes Swup with default options
-
 // Get current year for copyright
 {
   const yearText = document.querySelector(".year-text");
