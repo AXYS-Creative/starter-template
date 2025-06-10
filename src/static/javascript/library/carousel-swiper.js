cb2325ea26120574353be18787ac38ca8b05a964
Aaron Garcia <axyscreativeagency@gmail.com>
2025-06-09 20:26:45 -0600

    swiper carousel updates

--- src/static/javascript/library/carousel-swiper.js
+++ src/static/javascript/library/carousel-swiper.js
@@ -1,22 +1,37 @@
 // Requires external stylesheet (see app.njk)
-let swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 16,
-  loop: false,
-  cssMode: true,
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    1024: {
-      slidesPerView: 3,
+const carouselSwiper = document.querySelector(".carousel-swiper");
+
+if (carouselSwiper) {
+  const autoplayEnabled = carouselSwiper.dataset.autoplayEnabled === "true";
+  const autoplayInterval = carouselSwiper.dataset.autoplayInterval;
+  const paginationType = carouselSwiper.dataset.pagination;
+
+  let swiper = new Swiper(".mySwiper", {
+    autoplay: autoplayEnabled
+      ? {
+          delay: autoplayInterval,
+          disableOnInteraction: false,
+        }
+      : false,
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    loop: false,
+    mousewheel: true,
+    keyboard: true,
+    simulateTouch: true, // Allow drag (cssMode needs to be false)
+    cssMode: false,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+      },
     },
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-  },
-});
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: paginationType,
+    },
+  });
+}
