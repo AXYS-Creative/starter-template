61e56b19e06a74f2e211784bb25bcf307d988c8f
Aaron Garcia <axyscreativeagency@gmail.com>
2025-08-22 10:28:07 -0600

    alert a11y

--- src/static/javascript/library/alert.js
+++ src/static/javascript/library/alert.js
@@ -1,38 +1,46 @@
-const alertEls = document.querySelectorAll(".alert"),
-  alertTriggers = document.querySelectorAll(".alert-trigger");
+const alertEls = document.querySelectorAll(".alert");
+const alertTriggers = document.querySelectorAll(".alert-trigger");
 
-alertEls.forEach((el) => {
-  const closeBtn = el.querySelector(".alert__close-btn");
+const setAlertTabIndex = (alertEl, index) => {
+  const focusables = [
+    alertEl.querySelector(".alert__close-btn"),
+    alertEl.querySelector(".alert-content__link-1"),
+    alertEl.querySelector(".alert-content__link-2"),
+  ].filter(Boolean); // remove nulls
 
+  focusables.forEach((el) => (el.tabIndex = index));
+};
+
+const hideAlert = (alertEl) => {
+  alertEl.classList.remove("alert--active");
+  setAlertTabIndex(alertEl, -1);
+};
+
+const showAlert = (alertEl) => {
+  alertEl.classList.add("alert--active");
+  setAlertTabIndex(alertEl, 0);
+};
+
+alertEls.forEach((alertEl) => {
+  setAlertTabIndex(alertEl, -1);
+
+  const closeBtn = alertEl.querySelector(".alert__close-btn");
   if (closeBtn) {
-    closeBtn.tabIndex = -1;
-
-    closeBtn.addEventListener("click", () => {
-      el.classList.remove("alert--active");
-      closeBtn.tabIndex = -1;
-    });
+    closeBtn.addEventListener("click", () => hideAlert(alertEl));
   }
 });
 
 alertTriggers.forEach((trigger) => {
-  const triggerTarget = trigger.getAttribute("aria-controls");
-  const alertEl = document.getElementById(`alert-${triggerTarget}`);
-  const closeBtn = alertEl.querySelector(".alert__close-btn");
-  const alertAutoClose = alertEl.dataset.alertAutoclose;
+  const targetId = trigger.getAttribute("aria-controls");
+  const alertEl = document.getElementById(`alert-${targetId}`);
+  const autoClose = parseInt(alertEl.dataset.alertAutoclose, 10) || 0;
 
   trigger.addEventListener("click", (e) => {
     e.preventDefault();
+    showAlert(alertEl);
 
-    alertEl.classList.add("alert--active");
-
-    if (closeBtn) {
-      closeBtn.tabIndex = 0;
-    }
-
-    if (alertAutoClose > 0) {
-      setTimeout(() => {
-        alertEl.classList.remove("alert--active");
-      }, alertAutoClose);
+    if (autoClose > 0) {
+      setTimeout(() => hideAlert(alertEl), autoClose);
     }
   });
 });
