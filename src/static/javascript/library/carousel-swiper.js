e4e37f719230cb3a6623dbbb835936adfe41d919
Aaron Garcia <axyscreativeagency@gmail.com>
2025-10-21 07:33:42 -0600

    carousel-height variable, header link updates

--- src/static/javascript/library/carousel-swiper.js
+++ src/static/javascript/library/carousel-swiper.js
@@ -1,10 +1,14 @@
 // Requires external stylesheet (see app.njk)
+import { root } from "../util.js";
 const carouselSwiper = document.querySelector(".carousel-swiper");
 
 if (carouselSwiper) {
   const autoplayEnabled = carouselSwiper.dataset.autoplayEnabled === "true";
   const autoplayInterval = carouselSwiper.dataset.autoplayInterval;
   const paginationType = carouselSwiper.dataset.pagination;
+
+  let carouselHeight = carouselSwiper.offsetHeight;
+  root.style.setProperty("--carousel-swiper-height", `${carouselHeight}px`);
 
   let swiper = new Swiper(".mySwiper", {
     autoplay: autoplayEnabled
