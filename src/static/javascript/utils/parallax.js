fcd7aea1564715c2078a4a4cb734e70ece8d6b9f
Aaron Garcia <axyscreativeagency@gmail.com>
2026-04-06 21:31:05 -0600

    blog-list updates, parallax tweaks, variable changes

--- src/static/javascript/utils/parallax.js
+++ src/static/javascript/utils/parallax.js
@@ -1,17 +1,24 @@
 document.querySelectorAll(".parallax").forEach((el) => {
-  const dataY = el.dataset.parallaxY || "15%";
+  const dataYFrom = el.dataset.parallaxYFrom || "-5%";
+  const dataYTo = el.dataset.parallaxYTo || "5%";
   const dataScrub = parseFloat(el.dataset.parallaxScrub) || 1;
   const dataStart = el.dataset.parallaxStart || "top bottom";
   const dataEnd = el.dataset.parallaxEnd || "bottom top";
 
-  gsap.to(el, {
-    y: dataY,
-    ease: "none",
-    scrollTrigger: {
-      trigger: el,
-      start: dataStart,
-      end: dataEnd,
-      scrub: dataScrub,
+  gsap.fromTo(
+    el,
+    {
+      y: dataYFrom,
     },
-  });
+    {
+      y: dataYTo,
+      ease: "none",
+      scrollTrigger: {
+        trigger: el,
+        start: dataStart,
+        end: dataEnd,
+        scrub: dataScrub,
+      },
+    },
+  );
 });
